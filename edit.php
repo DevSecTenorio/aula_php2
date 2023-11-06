de0e65bc6d996f1a8ed34be9386c4ae181e38c94
Vitor Tenório <vitormendes023@outlook.com>
2023-11-06 14:23:06 -0300

    Melhorias Frontend edit and insert

--- edit.php
+++ edit.php
@@ -16,11 +16,30 @@
 $consulta = $conexao->query("SELECT * FROM pessoa where id=".$id);
 $linha = $consulta->fetch(PDO::FETCH_ASSOC);
 ?>
-<form method="post">
-    <input type="hidden" name="id" value="<?php echo $linha["id"];?>" />
-        <label>Qual é o seu nome?</label>
-    <input type='text' name='nome' placeholder='Qual é o seu nome?' value="<?php echo $linha["nome"];?>"><br>
-        <label>Qual é o seu e-mail?</label>
-    <input type='email' name='email' placeholder='Qual é o seu e-mail?' value="<?php echo $linha["email"];?>">
-        <input type='submit' value='salvar'>
-</form>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alterar Pessoa</title>
+    <link rel="icon" href="https://www.w3schools.com/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Alterar Pessoa</h1>
+        <form method="post">
+            <input type="hidden" name="id" value="<?php echo $linha["id"];?>" />
+            <div class="mb-3">
+                <label class="form-label">Qual é o seu nome?</label>
+                <input type='text' name='nome' placeholder='Qual é o seu nome?' class="form-control" value="<?php echo $linha["nome"];?>"><br>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Qual é o seu e-mail?</label>
+                <input type='email' name='email' placeholder='Qual é o seu e-mail?' class="form-control" value="<?php echo $linha["email"];?>">
+            </div>
+            <input type='submit' class="btn btn-primary" value='salvar'>
+        </form>
+    </div>
+</body>
+</html>
--- insert.php
+++ insert.php
@@ -13,10 +13,29 @@
         header("location: index.php");
     }
 ?>
-<form method="post">
-    <label>Qual é o seu nome?</label>
-    <input type='text' name='nome' placeholder='Qual é o seu nome?'><br>
-    <label>Qual é o seu e-mail?</label>
-    <input type='email' name='email' placeholder='Qual é o seu e-mail?'>
-    <input type='submit' value='salvar'>
-</form>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Adicionar Pessoa</title>
+    <link rel="icon" href="https://www.w3schools.com/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Adicionar Pessoa</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label class="form-label">Qual é o seu nome?</label>
+                <input type='text' class="form-control" name='nome' placeholder='Qual é o seu nome?'>
+            </div>    
+            <div class="mb-3">
+                <label class="form-label">Qual é o seu e-mail?</label>
+                <input type='text' class="form-control" name='email' placeholder='Qual é o seu email?'>
+            </div>
+            <input type='submit' value='salvar'>
+        </form>
+    </div>
+</body>
+</html>
